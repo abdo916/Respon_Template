0263a6f1c1948831d6b38335424a5c4439ffd664
unknown <abdallah.gibreel@gmail.com>
2020-08-12 18:03:22 +0200

    responsive site

---
+++ app.js
@@ -0,0 +1,34 @@
+jQuery (document).ready(function(){
+    "use strict";
+    $('#slide').carouFredSel ({
+        responsive: true,
+        width: '100%',
+        circular: true,
+        scroll: {
+            items:1,
+            duration:500,
+            pauseOnHover:true
+        },
+        auto:true,
+        items: {
+            visible: {
+                min:1,
+                max:1
+            },
+            height:"variable"
+        },
+        pagination: {
+            container:".sliderpage",
+            pageAnchorBuilder: false
+        }
+    });
+    $(window).scroll(function() {
+        var top =$(window).scrollTop();
+        if(top >= 60) {
+            $('header').addClass('secondary');
+        } else if ($('header').hasClass('secondary')) {
+            $('header').removeClass('secondary');
+        }
+
+    });
+});
